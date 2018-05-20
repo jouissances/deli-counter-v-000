99426c9047172430003d439233b10016d39edbf5
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 08:08:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{y + 1} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
