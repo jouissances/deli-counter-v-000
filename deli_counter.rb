1d01d5cffa0e4b5ee2f826e425ca39bc1594f93b
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 07:50:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,10 @@
 katz_deli = []
 
 def line(katz_deli)
+  x = katz_deli.map.with_index { |name, index| puts "#{index + 1}. #{name}" }
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    x = katz_deli.map.with_index { 
-      |name, index| 
-    puts "#{index + 1}. #{name}" 
-    }
     puts "#{x}"
   end
 end
