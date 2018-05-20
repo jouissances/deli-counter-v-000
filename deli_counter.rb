aa6f291537a42aa74e676ac340925aa3c03d5fe8
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 07:43:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    katz_deli.each_with_index { |name, index| puts "#{index}. #{name}" }
+  end
+end
+
+def take_a_number(katz_deli, name)
+end
+
+def now_serving
+end
